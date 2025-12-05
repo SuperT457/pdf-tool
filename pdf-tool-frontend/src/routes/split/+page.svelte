72bc0829a8b6bb331d3c8d719fbe2a337e201072
giulio <giulio.tosi.gt04@gmail.com>
2025-12-05 17:06:44 +0100

    implement file split and result download

--- pdf-tool-frontend/src/routes/split/+page.svelte
+++ pdf-tool-frontend/src/routes/split/+page.svelte
@@ -6,11 +6,30 @@
         file = e.target.files[0];
     }
 
-    function upload(){
+    async function split(){
         if (!file){
-            return alert("Provide a file!")
+            return alert("Provide a file!");
         }
-        alert("File uploaded correctly")
+        
+        const formData = new FormData();
+        formData.append("file",file);
+
+        const res = await fetch("http://localhost:8000/api/split",{method:"POST",body:formData});
+        
+        if (!res.ok){
+            return alert("Error while processing PDF");
+        }
+
+        const blob = await res.blob();
+
+        const url = URL.createObjectURL(blob);
+
+        const link = document.createElement("a");
+        link.href = url;
+        // link.download = "download.pdf";
+        link.click();
+
+        URL.revokeObjectURL(url);
     }
 
 </script>
@@ -19,4 +38,4 @@
 <p>Upload your document and cut only the pages you need</p>
 
 <input type="file" accept="application/pdf" on:change={handleFile} />
-<button on:click={upload}>Upload</button>
+<button on:click={split}>Upload</button>
